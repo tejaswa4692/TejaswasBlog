bd3f1e6491d32f352c48bc7c3e8238f27d32ec01
unknown <tejaswa4692@gmail.com>
2025-10-19 22:13:06 +0530

    edit5

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+
+function addstuff(heading, paragraph, date){
+    const newDiv = document.getElementById("card");
+    const cloned = document.createElement("div");
+    cloned.innerHTML = newDiv.innerHTML;
+
+
+    cloned.querySelector("h2").textContent = heading;
+    cloned.querySelector("p").textContent = paragraph;
+    cloned.querySelector("h5").textContent = date;
+
+    cloned.classList = "card"
+    document.getElementById("my-div").insertBefore(cloned, document.getElementById("my-div").firstChild)
+}
+
+
+
+async function initload() {
+    const res = await fetch('https://tejaswablog.onrender.com');
+    const data = await res.json();
+    
+    data.cards.forEach(c => {
+        addstuff(c.heading, c.content, c.date);
+    });
+
+}
+
+
+window.onload = initload()
