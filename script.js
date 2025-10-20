9c5d6478d8f286c0709a7a71e897527e114b3245
tejaswa4692 <tejaswa4692@gmail.com>
2025-10-20 11:42:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function addstuff(heading, paragraph, date){
 
 
 async function initload() {
-    const res = await fetch('https://tejaswablog.onrender.com/cards');
+    const res = await fetch('https://bckend-dbam.onrender.com/cards');
     const data = await res.json();
     
     data.cards.forEach(c => {
